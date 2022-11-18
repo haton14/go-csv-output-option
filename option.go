f58deef47826411979161e27043bd17a53368f4b
haton14 <iioka.takumi.quena@gmail.com>
2022-11-18 11:55:10 +0900

    ignore field

--- option.go
+++ option.go
@@ -16,11 +16,15 @@ func ParseOption(opt any) (*Option, error) {
 	parse := make(map[string]bool, count)
 	value := reflect.ValueOf(opt)
 	for i := 0; i < count; i++ {
+		tagValue := reflect.TypeOf(opt).Field(i).Tag.Get(tagName)
+		if tagValue == "" {
+			continue
+		}
 		optionFiledType := value.Field(i).Kind()
 		if optionFiledType != reflect.Bool {
 			return nil, fmt.Errorf("option filed expected type is bool. has type %s", optionFiledType)
 		}
-		parse[reflect.TypeOf(opt).Field(i).Tag.Get(tagName)] = value.Field(i).Bool()
+		parse[tagValue] = value.Field(i).Bool()
 	}
 	return &Option{
 		value: parse,
